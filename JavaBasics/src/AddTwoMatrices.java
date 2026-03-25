0a1cae6ee3b0e6bd302e69201dc6dfc0907b0a08
myxley <caonguyentrivinh@gmail.com>
2026-03-25 16:59:37 +0700

    bai 6.6

---
+++ JavaBasics/src/AddTwoMatrices.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class AddTwoMatrices {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
+        int m = sc.nextInt();
+        System.out.print("Enter columns: ");
+        int n = sc.nextInt();
+
+        int[][] A = new int[m][n];
+        int[][] B = new int[m][n];
+        int[][] C = new int[m][n];
+
+        System.out.println("Enter matrix A:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("A[" + i + "][" + j + "] = ");
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter matrix B:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("B[" + i + "][" + j + "] = ");
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        System.out.println("Result matrix C:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
+	}
+}
